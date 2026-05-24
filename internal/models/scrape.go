10c9b69f606aa433d2db09bce778a75b1069233f
0xrinful <0xrinful@gmail.com>
2026-05-24 20:46:17 +0300

    feat: add data models

---
+++ internal/models/scrape.go
@@ -0,0 +1,38 @@
+package models
+
+import "time"
+
+type Manga struct {
+	Slug        string
+	SourceID    string
+	Title       string
+	Description string
+	CoverURL    string
+	Status      string
+	Genres      []string
+	Chapters    []Chapter
+}
+
+type Chapter struct {
+	URL        string
+	MangaSlug  string
+	SourceID   string
+	Number     float64
+	Title      string
+	ReleasedAt time.Time
+	Pages      []Page
+}
+
+type ChapterRange struct {
+	From float64
+	To   float64
+}
+
+func (r ChapterRange) Contains(number float64) bool {
+	return number >= r.From && number <= r.To
+}
+
+type Page struct {
+	Number int
+	URL    string
+}
---
+++ internal/models/store.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type MangaRecord struct {
+	Manga
+	CoverPath string
+	AddedAt   time.Time
+	UpdatedAt time.Time
+}
+
+type ChapterRecord struct {
+	Chapter
+	RawPath       string
+	OptimizedPath string
+	CBZPath       string
+	Downloaded    bool
+	DownloadedAt  time.Time
+	Optimized     bool
+	OptimizedAt   time.Time
+}
